52600c5a1b963d140f75e4721dca0952312f6a43
David Dever <david.dever@cox.net>
2014-04-12 14:53:39 -0700

    Restructure with classes

--- dungeon.py
+++ dungeon.py
@@ -1,48 +1,82 @@
 import random
 
-def roll_dice():
-    d6 = random.randint(1, 6)
-    return d6
-
 def check_spot(spot):
     if spot in dungeon:
         event_happens = dungeon[spot] - 1000
         print event_text[event_happens]
 
-def game():
+class Player:
+
+    def __init__(self, spot, hp, key, talisman, potion1, potion2, pole):
+        self.spot = 27
+        self.hp = 3
+        self.key = 0
+        self.talisman = 0
+        self.potion1 = 0
+        self.potion2 = 0
+        self.pole = 0
+
+    def move_player(self, spots):
+        self.spot += spots
+
+    def take_damage(self, dmg):
+        self.hp += dmg
+        if self.hp <= 0:
+            Game.end()
+
+    def add_item(self, item):
+        setattr(self, item, True):
+        if item.startswith('potion'):
+            self.hp += 2
+
+    def lower_pole(self):
+        pole = True
+   
+    def has_item(self, item):
+        return gettattr(self, item)
+
+    def pole_down(self):
+        return self.pole
+
+    def current_spot():
+        return self.spot
+
+class Game:
+
+    def __init__(self):    
+        print 'A new Doomed Soul enters the Deadly Danger Dungeon'
+   
+    def check_spot(self, spot):
+        if Player.current_spot in dungeon:
+            event_happens = dungeon[spot] - 1000
+            print event_text[event_happens]
     
-    # Set variables for new game
-    spot = 27
-    hp = 3
-    key = 0
-    talisman = 0
-    potion1 = 0
-    potion2 = 0
-    pole = 0
-    
-    print 'A new Doomed Soul enters the Deadly Danger Dungeon'
-    while hp > 0:        
-        
+    def determine_direction(self, key, spot, pole)  
         # Determine which direction to go based on game conditions
-        if key == 1 and spot > 1 or spot > 89 and pole == 1:
+        if Player.has_item('key') == True and Player.current_spot > 1 or Player.current_spot > 89 and Player.pole_down == True:
             # Special case, going backwards
-            roll = roll_dice() * -1
-            spot = spot + roll
-            print spot
-            print roll
-            check_spot(spot)
-        
+            self.direction = 0
+       
         else:
             # Normal progression
-            roll = roll_dice() 
-            spot = spot + roll
-            print spot
-            print roll
-            check_spot(spot)
+            self.direction = 1
+   
+    def move_spots(self):
+        roll = roll_dice()
+        if self.direction == 0:
+            # Moving backwards
+            roll = roll * -1
+        return roll    
 
+
+    def check_win(self, spot)
         if spot > 100:
             print 'You escaped or something'
-            hp = 0
+    
+def roll_dice():
+    d6 = random.randint(1, 6)
+    return d6
+
 
 dungeon = {1:1001, 6:1002, 8:1003, 10:1004, 11:1005, 13:1006, 16:1006, 19:1005, 21:1004, 23:1003, 24:1003, 29:1005, 30:1005, 35:1005, 40:1007, 43:1003, 44:1008, 45:1003, 48:1013, 52:1005, 54:1015, 55:1015, 56:1005, 58:1008, 63:1009, 66:1016, 69:1005, 71:1005, 73:1005, 75:1010, 79:1010, 83:1011, 84:1011, 85:1011, 86:1011, 88:1011, 102:1014, 104:1012}
 
@@ -53,4 +87,4 @@ wins = 0
 deaths = 0
 games = 0
 
-game()
+)
